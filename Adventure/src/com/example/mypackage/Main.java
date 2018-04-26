b0d9d742c721eaf8afaa69fd191919f41bba55f6
sajfrahani <sajjfarahani@outlook.com>
2018-04-26 19:58:58 +1000

    Adventure Project

--- Adventure/src/com/example/mypackage/Main.java
+++ Adventure/src/com/example/mypackage/Main.java
@@ -21,60 +21,61 @@ public class Main {
         locations.get(1).addExist("E", 3);
         locations.get(1).addExist("S", 4);
         locations.get(1).addExist("N", 5);
-//        locations.get(1).addExist("Q", 0);
 
         locations.get(2).addExist("N", 5);
-//        locations.get(2).addExist("Q", 0);
 
         locations.get(3).addExist("W", 1);
-//        locations.get(3).addExist("Q", 0);
 
         locations.get(4).addExist("N", 1);
         locations.get(4).addExist("W", 2);
-//        locations.get(4).addExist("Q", 0);
 
         locations.get(5).addExist("S", 1);
         locations.get(5).addExist("W", 2);
-//        locations.get(5).addExist("Q", 0);
+
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
 
         int loc =1;
-//        while (true){
-//            System.out.println(locations.get(loc).getDescription());
-//            if(loc == 0){
-//                break;
-//            }
-//
-//            Map<String , Integer> exists = locations.get(loc).getExits();
-//            System.out.println("Available exists are ");
-//
-//            for(String exit: exists.keySet()){
-//                System.out.print(exit + ", ");
-//            }
-//            System.out.println();
-//            String direction = scanner.nextLine().toUpperCase();
-//
-//            if (exists.containsKey(direction)) {
-//                loc = exists.get(direction);
-//
-//            } else {
-//                System.out.println("You cannot go in that direction.");
-//            }
-//
-//            }
-        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
-        for (String i : road) {
-            System.out.println(i);
-        }
+        while (true){
+            System.out.println(locations.get(loc).getDescription());
+            if(loc == 0){
+                break;
+            }
 
-        System.out.println("==================================");
+            Map<String , Integer> exists = locations.get(loc).getExits();
+            System.out.println("Available exists are ");
 
-        String[] building = "You are inside a building, a well house for a small spring".split(", ");
-        for (String i : building) {
-            System.out.println(i);
-        }
-    }
+            for(String exit: exists.keySet()){
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+            String direction = scanner.nextLine().toUpperCase();
+            if(direction.length() > 1){
+                String[] words = direction.split(" ");
+                for(String word: words){
+                    if(vocabulary.containsKey(word)){
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
 
-        }
+            if (exists.containsKey(direction)) {
+                loc = exists.get(direction);
+
+            } else {
+                System.out.println("You cannot go in that direction.");
+            }
+
+            }
 
     }
+}
+
+
 
