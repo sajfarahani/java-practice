21cdf68c40b5f1d66ef8893b453d11805e7a736e
sajfrahani <sajjfarahani@outlook.com>
2018-05-29 22:21:28 +1000

    sortedCollections Practice

--- sortedCollection/src/com/example/mypackage/Main.java
+++ sortedCollection/src/com/example/mypackage/Main.java
@@ -1,8 +1,88 @@
 package com.example.mypackage;
 
+import java.util.Map;
+
 public class Main {
 
+    private static StockList stockList = new StockList();
+
     public static void main(String[] args) {
-	// write your code here
+       StockItem temp = new StockItem("Bread", 0.86, 100);
+       stockList.addStock(temp);
+
+       temp = new StockItem("cake", 1.10, 7);
+       stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62.0, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.50, 200);
+        stockList.addStock(temp);
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.50, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.40, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+
+        for(String s: stockList.Items().keySet()){
+            System.out.println(s);
+        }
+
+        Basket sajjBasket = new Basket("Sajj");
+        sellItem(sajjBasket, "car", 1);
+        System.out.println(sajjBasket);
+
+        sellItem(sajjBasket, "car", 1);
+        System.out.println(sajjBasket);
+
+//        sellItem(sajjBasket, "car" , 1);
+        sellItem(sajjBasket, "spanner", 5);
+        System.out.println(sajjBasket);
+
+        sellItem(sajjBasket, "juice", 4);
+        sellItem(sajjBasket, "cup", 12);
+        sellItem(sajjBasket, "Bread", 1);
+
+        System.out.println(stockList);
+
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+
+        stockList.Items().get("Car").adjustStock(2000);
+        stockList.get("Car").adjustStock(-1000);
+        System.out.println(stockList);
+        for(Map.Entry<String, Double> price: stockList.PriceList().entrySet()){
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }
+
     }
+
+    public static int sellItem(Basket basket, String item, int quantity){
+        //retrieve the item from stock List
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null){
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(stockList.sellStock(item, quantity) != 0){
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
 }
--- sortedCollection/src/com/example/mypackage/StockList.java
+++ sortedCollection/src/com/example/mypackage/StockList.java
@@ -1,21 +1,22 @@
 package com.example.mypackage;
 
+import java.security.PublicKey;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StockList {
 
     private final Map<String, StockItem> list;
 
-    public StockList(Map<String, StockItem> list) {
-        this.list = new HashMap<>();
+    public StockList() {
+        this.list = new LinkedHashMap<>();
     }
 
     public int addStock(StockItem item){
         if(item!=null){
             //Check if already have quantities of this item
-            StockItem intStcok = list.get(item.getName(), item);
+            StockItem intStcok = list.getOrDefault(item.getName(), item);
             //If there are already stocks on this item, adjust the quantity
             if(intStcok !=item){
                 item.adjustStock(intStcok.quantityInStock());
@@ -38,6 +39,15 @@ public class StockList {
     public StockItem get(String key){
         return list.get(key);
     }
+
+    public Map<String, Double> PriceList(){
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for(Map.Entry<String, StockItem> item: list.entrySet()){
+            prices.put(item.getKey(), item.getValue().getPrice());
+        }
+        return Collections.unmodifiableMap(prices);
+    }
+
     public Map<String, StockItem> Items(){
         return Collections.unmodifiableMap(list);
     }
@@ -52,7 +62,7 @@ public class StockList {
             double itemValue = stockItem.getPrice()*stockItem.quantityInStock();
 
             s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
-            s = s + itemValue + "\n";
+            s = s + String.format("%.2f",itemValue) + "\n";
             totalCost += itemValue;
         }
         return s + "Total stock value" + totalCost;
