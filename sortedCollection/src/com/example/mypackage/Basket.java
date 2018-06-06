a99c962a6084f2b0f2b4f0b2c677e1bc1deeac06
sajfrahani <sajjfarahani@outlook.com>
2018-06-06 21:31:47 +1000

    Sorted Collection Challenge

--- sortedCollection/src/com/example/mypackage/Basket.java
+++ sortedCollection/src/com/example/mypackage/Basket.java
@@ -13,8 +13,8 @@ public class Basket {
         this.list = new TreeMap<>();
     }
 
-    public int addToBasket(StockItem item, int quantity){
-        if((item != null) && (quantity > 0)){
+    public int addToBasket(StockItem item, int quantity) {
+        if ((item != null) && (quantity > 0)) {
             int inBasket = list.getOrDefault(item, 0);
             list.put(item, inBasket + quantity);
             return inBasket;
@@ -22,18 +22,39 @@ public class Basket {
         return 0;
     }
 
-    public Map<StockItem, Integer> Items(){
+    public int removeFromBasket(StockItem item, int quantity) {
+        if((item != null) && (quantity > 0)) {
+            // check if we already have the item in the basket
+            int inBasket = list.getOrDefault(item, 0);
+            int newQuantity = inBasket - quantity;
+
+            if(newQuantity > 0) {
+                list.put(item, newQuantity);
+                return quantity;
+            } else if(newQuantity == 0) {
+                list.remove(item);
+                return quantity;
+            }
+        }
+        return 0;
+    }
+
+    public void clearBasket() {
+        this.list.clear();
+    }
+
+    public Map<StockItem, Integer> Items() {
         return Collections.unmodifiableMap(list);
     }
 
     @Override
     public String toString() {
-        String s = "\nShopping basket " + name + " contains " + list.size() +  ((list.size() == 1) ? " item" : " items") +  " \n";
+        String s = "\nShopping basket " + name + " contains " + list.size() + ((list.size() == 1) ? " item" : " items") + "\n";
         double totalCost = 0.0;
-        for(Map.Entry<StockItem, Integer> item : list.entrySet()){
+        for (Map.Entry<StockItem, Integer> item : list.entrySet()) {
             s = s + item.getKey() + ". " + item.getValue() + " purchased\n";
-            totalCost += item.getKey().getPrice()*item.getValue();
+            totalCost += item.getKey().getPrice() * item.getValue();
         }
-        return s + "Toatal cost " + totalCost;
+        return s + "Total cost " + totalCost;
     }
 }
