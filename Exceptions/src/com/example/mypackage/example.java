bb42f59275891b2e9d3fef78f141d8115a7e5c19
sajfrahani <sajjfarahani@outlook.com>
2018-06-11 18:33:51 +1000

    Exception handler

--- Exceptions/src/com/example/mypackage/example.java
+++ Exceptions/src/com/example/mypackage/example.java
@@ -1,24 +1,45 @@
 package com.example.mypackage;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try{
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e){
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division");
+        }
     }
 
     private static int divide(){
-        int x = getInt();
-        int y = getInt();
-        System.out.println("x is " + x + ", y is " + y);
-        return x / y;
+        int x, y;
+//        try{
+            x = getInt();
+            y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x/y;
+//        } catch (NoSuchElementException e){
+//            throw new ArithmeticException("no suitable input");
+//        } catch (ArithmeticException e){
+//            throw new ArithmeticException("Attempt to divide by zero");
+//        }
     }
 
     private static int getInt(){
         Scanner s = new Scanner(System.in);
         System.out.println("Please entre an integer ");
-        return s.nextInt();
+        while(true){
+            try{
+                return s.nextInt();
+            }catch (InputMismatchException e){
+                s.nextLine();
+                System.out.println("Please entre a number only the digits 0 to 9");
+            }
+        }
     }
 }
