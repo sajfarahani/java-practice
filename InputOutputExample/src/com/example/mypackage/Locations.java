2ad74b4fc4d3ddc30a16a129a74d016052ac0675
sajfrahani <sajjfarahani@outlook.com>
2018-06-13 20:53:45 +1000

    InputOutput example

--- InputOutputExample/src/com/example/mypackage/Locations.java
+++ InputOutputExample/src/com/example/mypackage/Locations.java
@@ -1,5 +1,6 @@
 package com.example.mypackage;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -51,6 +52,36 @@ public class Locations implements Map<Integer, Location> {
         } catch (IOException e){
 
         } finally {
+            if(scanner != null){
+                scanner.close();
+            }
+        }
+
+        // Now read the exits
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+            scanner.useDelimiter(",");
+            while(scanner.hasNextLine()) {
+//                int loc = scanner.nextInt();
+//                scanner.skip(scanner.delimiter());
+//                String direction = scanner.next();
+//                scanner.skip(scanner.delimiter());
+//                String dest = scanner.nextLine();
+//                int destination = Integer.parseInt(dest);
+                String input = scanner.nextLine();
+                String[] data = input.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction, destination);
+            }
+
+            } catch (IOException e){
+                e.printStackTrace();
+            } finally {
             if(scanner != null){
                 scanner.close();
             }
