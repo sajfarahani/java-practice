db195e6e700a2d3ca35188a4c592165497b8688f
sajfrahani <sajjfarahani@outlook.com>
2018-07-15 22:12:29 +1000

    Adding ByteStream

--- InputOutputExample/src/com/example/mypackage/Locations.java
+++ InputOutputExample/src/com/example/mypackage/Locations.java
@@ -7,13 +7,18 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
 
     public static void main(String[] args) throws IOException {
-        try(BufferedWriter locFile = new BufferedWriter(new FileWriter("locations.txt"));
-            BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions.txt"))) {
-            for(Location location : locations.values()) {
-                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-                for(String direction : location.getExits().keySet()) {
-                    if(!direction.equalsIgnoreCase("Q")) {
-                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+        try(DataOutputStream logFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))){
+            for(Location location : locations.values()){
+                logFile.writeInt(location.getLocationID());
+                logFile.writeUTF((location.getDescription()));
+                System.out.println("Writing location "+ location.getLocationID()+ " :" + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size() -1) + " exits.");
+                logFile.writeInt(location.getExits().size() -1);
+                for(String direction: location.getExits().keySet()){
+                    if(!direction.equalsIgnoreCase("Q")){
+                        System.out.println("\t\t"+ direction + "," + location.getExits().get(direction));
+                        logFile.writeUTF(direction);
+                        logFile.writeInt(location.getExits().get(direction));
                     }
                 }
             }
