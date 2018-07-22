5fbbbb58dcfd75fcfbba765574ce369d632a35cd
sajfrahani <sajjfarahani@outlook.com>
2018-07-22 20:32:41 +1000

    Adding lambda expression

---
+++ ButtonFX/src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button clickMeButton;
+
+    public void initialize(){
+        clickMeButton.setOnAction(event -> System.out.println("You Clicked Me!"));
+//        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("You clicked me!");
+//            }
+//        });
+    }
+
+}
---
+++ ButtonFX/src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    //Map a method to lambda expression
+    //1. The method parameter becomes the argument list
+    //2. The arrow token is always there
+    //3. The body of the method becomes the body of the lambda expression
+     
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
